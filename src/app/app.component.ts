0edb7ebac4a55251b8dedb170c206446b0e2cbab
Ludron <ludron@gmail.com>
2022-01-06 01:34:17 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { Template } from '@angular/compiler/src/render3/r3_ast';
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+@Component(
+  {selector: 'app-root',
+  templateUrl: 'app.component.html'
+  
+  }
+)
+
 export class AppComponent {
-  title = 'bases';
+ 
 }
